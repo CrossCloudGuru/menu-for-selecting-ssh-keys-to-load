5a2fd3d02ce41ffc6954d0ed6ec90119b0861f6c
Cross Cloud Guru <70333421+CrossCloudGuru@users.noreply.github.com>
2020-11-01 15:11:38 +0100

    Initial Commit of script

---
+++ add-ssh.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Author: Marco Tijbout - CrossCloud.Guru
+# License: GPL v2.0
+# Version: 1
+
+SOURCE_DIR="${HOME}/.ssh"
+ARR_FILES=( $(grep -d skip -l "BEGIN" ${SOURCE_DIR}/*) )
+ARR_FILES+=( "Exit selection ..." )
+
+fnAddSSH() {
+    echo -e "Adding SSH Key $option"
+    ssh-add $option
+}
+
+fnCreateMenu () {
+    select option; do # in "$@" is the default
+        if [ "$REPLY" -eq "$#" ];
+        then
+            echo -e "Exiting..."
+            break;
+        elif [ 1 -le "$REPLY" ] && [ "$REPLY" -le $(($#-1)) ];
+        then
+            echo -e "You selected $option which is option $REPLY"
+            fnAddSSH
+            break;
+        else
+            echo -e "Incorrect Input: Select a number 1-$#"
+        fi
+    done
+}
+
+fnCreateMenu "${ARR_FILES[@]}"
